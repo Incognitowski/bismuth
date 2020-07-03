09c013767e2b6e4cf4d974ec63338dd6cff9a6cf
Junior Zytkowski <junior.zytkowski@gmail.com>
2020-07-02 22:15:59 -0300

    Added fake logo

--- src/views/authentication/Login.vue
+++ src/views/authentication/Login.vue
@@ -1,9 +1,24 @@
 <template>
-    
+    <v-card
+            dark
+            class="mx-auto"
+            max-width="344"
+            outlined
+    >
+        <img :src="require('../../assets/logo.svg')">
+
+        <v-card-actions>
+            <v-btn text>Button</v-btn>
+            <v-btn text>Button</v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
-<script>
-    export default {
-        name: "Login"
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class Login extends Vue {
+
     }
 </script>
